27338c00bd2ec78a5acc0fcec4b8f4560bbac5f7
Mohamed Sahad K P <mohamed.sahad@experionglobal.com>
2019-02-11 20:44:10 +0530

    added route in http module

--- example/http/app.js
+++ example/http/app.js
@@ -1,9 +1,19 @@
 const http = require('http');
 
-const server = http.createServer();
-server.on('connection', (socket) => {
-    console.log('New Conntection', socket);
+const server = http.createServer((req, res) => {
+    if(req.url == '/') {
+        res.write('Hello World');
+        res.end();
+    }
+    if(req.url == '/api') {
+        res.write(JSON.stringify([1 , 2, 3, 4 ]));
+        res.end();
+    }
 });
+
+// server.on('connection', (socket) => {
+//     console.log('New Conntection', socket);
+// });
 server.listen(3000);
 
 console.log(`Listening on port 3000`);
