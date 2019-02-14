815fe3611f151d1d4916825206c2924c89481698
Mohamed Sahad K P <mohamed.sahad@experionglobal.com>
2019-02-14 11:42:53 +0530

    added get and post route in express mosule

--- example/expressjs/app.js
+++ example/expressjs/app.js
@@ -1,14 +1,38 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
+const courses = [
+    { id : 1, name : "Course1" },
+    { id : 2, name : "Course2" },
+    { id : 3, name : "Course3" }
+]
+
 app.get('/', (req, res) => {
     res.send('Hello Wolrd')
 });
 
 app.get('/api/course', (req, res) => {
-    res.send([2, 3, 5]);
-})
+    res.send(courses);
+});
 
-app.listen(3000, () => {
-    console.log('Listen port 3000....');
+app.post('/api/course', (req, res) => {
+    const course = {
+        id : courses.length + 1,
+        name : req.body.name
+    }
+    courses.push(course);
+    res.send(course);
+});
+
+app.get('/api/course/:id', (req, res) => {
+    let course = courses.find(c => c.id === parseInt(req.params.id));
+    if(!course) res.sendStatus(404).send("Not Found");
+    res.send(course);
+});
+
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+    console.log(`Listening port ${port}....`);
 })
